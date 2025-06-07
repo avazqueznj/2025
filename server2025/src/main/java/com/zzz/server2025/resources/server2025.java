75139f7325f990d0338efbc7f27545b759bf30d3
alejandro <alejandrovazquez@yahoo.com>
2025-06-07 00:33:17 -0700

    working test 1

--- server2025/src/main/java/com/zzz/server2025/resources/server2025.java
+++ server2025/src/main/java/com/zzz/server2025/resources/server2025.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package com.zzz.server2025.resources;
 
+import java.util.logging.Logger;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -17,10 +14,18 @@ public class server2025 extends HttpServlet {
 
 
 
+    
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(
+            HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        Logger logger = Logger.getLogger(getClass().getName());
+        logger.info("GETCONFIG: Start....");
+            
+        
+        logger.info("GETCONFIG: Send....");
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -31,8 +36,11 @@ public class server2025 extends HttpServlet {
             out.println("zone1component1=right blinker");
             out.println("zone1component2=left blinker");
             out.println("end");
+            out.close();
+            
+            logger.info("GETCONFIG: Send.... DONE!\r\n");            
         }
-
+        
         
     }
 
