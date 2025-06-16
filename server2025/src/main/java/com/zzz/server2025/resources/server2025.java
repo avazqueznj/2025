9c73635b6442f39afea560b6c21b7e94468d835e
alejandro <alejandrovazquez@yahoo.com>
2025-06-15 23:21:17 -0700

    Server version 1, only config

--- server2025/src/main/java/com/zzz/server2025/resources/server2025.java
+++ server2025/src/main/java/com/zzz/server2025/resources/server2025.java
@@ -1,5 +1,7 @@
 package com.zzz.server2025.resources;
 
+import KFramework30.Base.KMetaUtilsClass;
+import KFramework30.Base.persistentTextClass;
 import java.util.logging.Logger;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,42 +10,47 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Iterator;
 
 @WebServlet(name = "config", urlPatterns = {"/config"})
 public class server2025 extends HttpServlet {
-
-
-
-    
     
     @Override
     protected void doGet(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+              
         Logger logger = Logger.getLogger(getClass().getName());
         logger.info("GETCONFIG: Start....");
+                
+        try{
             
-        
-        logger.info("GETCONFIG: Send....");
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("config:2025v1");
-            out.println("version:1");
-            out.println("type:truck");
-            out.println("zones:1");
-            out.println("zone1component1=right blinker");
-            out.println("zone1component2=left blinker");
-            out.println("end");
-            out.close();
+            logger.info("GETCONFIG: Send....");
             
-            logger.info("GETCONFIG: Send.... DONE!\r\n");            
-        }
-        
-        
-    }
+            logger.info("GETCONFIG: Loading....");            
+            persistentTextClass configFile = new persistentTextClass( "EVIRconfig1.txt" );
+            configFile.loadFromDisk();                        
+            logger.info("GETCONFIG: Loading.... DONE");
+            
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter writer = response.getWriter();
+            
+            //write the response
+            Iterator configFileLine = configFile.iterator();
+            while( configFileLine.hasNext() ){
+                writer.println( configFileLine.next() );
+            }
 
+            writer.close();
+            logger.info("GETCONFIG: Send.... DONE!\r\n");            
+                                               
+        }catch( Exception error ){
+            logger.severe(
+                    KMetaUtilsClass.getStackTrace(error) );
+        }
+                        
+    }
 
 }
 
